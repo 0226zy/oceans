34e6c98f00b2ea2d7ed07fc8a18e24df15230838
0226zy <grbh0226@gmail.com>
2019-11-18 21:46:57 +0800

    add

--- main.go
+++ main.go
@@ -12,11 +12,11 @@ func main() {
 	router := routers.InitRouter()
 
 	s := &http.Server{
-		Addr:		fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler:	router,
-		ReadTimeout:	setting.ReadTimeout,
-		WriteTimeout:	setting.WriteTimeout,
-		MaxHeaderBytes:	1 << 20,
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
 	}
 	s.ListenAndServe()
 }
